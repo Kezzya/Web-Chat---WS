26c0ea6f73bc63513eb6b19c35070fca9bd42216
Nikita <kobzevnikita0@mail.ru>
2022-08-29 14:32:00 +0300

    added ejs and nodemon

--- src/index.js
+++ src/index.js
@@ -3,29 +3,35 @@ const app = express();
 
 const path = require("path");
 
-// const dirPath = __dirname
-// const pathStatic = __dirname.length[__dirname.length - ];
-app.use(express.static("../static"));
+const dirPath = path.join(__dirname, "../");
+
+// app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+app.set("views", dirPath + "static\\views");
+
+app.use(express.static(dirPath + "static"));
+
 app.get("/", (req, resp) => {
-  resp.sendFile(path.join(__dirname, "../") + `static/index.html`);
+  resp.render(`index`);
 });
 app.get("/chats", (req, resp) => {
-  resp.sendFile(path.join(__dirname, "../") + `static/chats.html`);
+  resp.render(`chats`);
 });
 app.get("/profile", (req, resp) => {
-  resp.sendFile(path.join(__dirname, "../") + `static/profile.html`);
-});
-app.get("/*", (req, resp) => {
-  resp.sendFile(path.join(__dirname, "../") + `static/404.html`);
+  resp.render(`profile`);
 });
 
-app.get("/500", (req, resp) => {
-  resp.sendFile(path.join(__dirname, "../") + `static/500.html`);
-});
 app.get("/registration", (req, resp) => {
-  resp.sendFile(path.join(__dirname, "../") + `static/registration.html`);
+  resp.render(`registration`);
 });
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is up on port: ${port}`);
+app.get("/500", (req, resp) => {
+  resp.render(`500`);
+});
+app.get("/*", (req, resp) => {
+  resp.render(`404`);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is up on port: ${PORT}`);
 });
